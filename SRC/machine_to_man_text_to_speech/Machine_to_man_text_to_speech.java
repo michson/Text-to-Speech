880b5f997c077a5f16940184c4d478bede52dea4
Michael Olaoye <prog4michpolite@gmail.com>
2022-09-06 21:12:09 +0100

    first commit

---
+++ SRC/machine_to_man_text_to_speech/Machine_to_man_text_to_speech.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machine_to_man_text_to_speech;
+
+/**
+ *
+ * @author OLUWASEGUN
+ */
+public class Machine_to_man_text_to_speech {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
