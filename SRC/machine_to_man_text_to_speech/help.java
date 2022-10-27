449b61321652a2d114f729b4e98b807d72f08055
Olaoye Mich <41102062+michson@users.noreply.github.com>
2022-10-27 10:11:55 +0100

    Update help.java

--- SRC/machine_to_man_text_to_speech/help.java
+++ SRC/machine_to_man_text_to_speech/help.java
@@ -6,6 +6,7 @@ public class help extends javax.swing.JFrame {
     public help() {
         initComponents();
     }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
